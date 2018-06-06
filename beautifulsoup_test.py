e835896635cb601785af5717f80b149cd4dadee7
joyce0101 <13759969768@163.com>
2018-06-06 21:35:04 +0800

    learn BeautifulSoup

--- beautifulsoup_test.py
+++ beautifulsoup_test.py
@@ -1,3 +1,4 @@
+import bs4
 from bs4 import BeautifulSoup
 
 
@@ -5,6 +6,7 @@ from bs4 import BeautifulSoup
 Beautiful Soup 4.4官方文档连接：http://beautifulsoup.readthedocs.io/zh_CN/latest/
 Beautiful Soup 是一个可以从HTML或XML文件中提取数据的Python库.
 它能够通过你喜欢的转换器实现惯用的文档导航,查找,修改文档的方式.Beautiful Soup会帮你节省数小时甚至数天的工作时间.
+推荐使用lxml解析器，它速度更快，功能更加强大，其他解析器查看官方文档。
 """
 
 
@@ -25,7 +27,7 @@ def test():
     <p class="story">...</p>
     '''
     soup = BeautifulSoup(html, 'lxml')
-    print(soup.prettify())
+    print(soup.prettify())  # prettify()函数格式化输出，经常用到，需要记好
     print(soup.title)
     print(soup.title.name)
     print(soup.title.string)
@@ -35,3 +37,108 @@ def test():
     print(soup.a)
     print(soup.find_all('a'))
     print(soup.find(id='link3'))
+
+
+"""
+Beautiful Soup将复杂HTML文档转换成一个复杂的树形结构，每个节点都是一个python对象，所有对象可以归纳为四种：
+1、Tag 2、NavigableString 3、Beautiful Soup 4、Comment
+下面的代码进行举例。
+"""
+
+
+def soup_test():
+    html = '''
+        <html><head><title>The Dormouse's story</title></head>
+        <body>
+        <p class="title" name="dromouse"><b>The Dormouse's story</b></p>
+        <p class="story">Once upon a time there were three little sisters; and their names were
+        <a href="http://example.com/elsie" class="sister" id="link1">!--Elsie--</a>,
+        <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+        <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+        and they lived at the bottom of a well.</p>
+        <p class="story">...</p>
+        '''
+    soup = BeautifulSoup(html, 'lxml')
+    # 1、tag对象的使用，即HTML文档中的标签
+    print(soup.title)  # <title>The Dormouse's story</title>
+    print(soup.head)  # <head><title>The Dormouse's story</title></head>
+    print(soup.a)  # <a class="sister" href="http://example.com/elsie" id="link1">Elsie</a>，需要注意它查找第一个符合要求的标签
+    print(type(soup.title))  # 验证对象的类型， <class 'bs4.element.Tag'>
+    # 对于tag，他有两个重要的属性，name和attrs
+    print(soup.name)  # [document]
+    print(soup.head.name)  # head ,输出标签本身的名字
+    print(soup.p.attrs)  # {'class': ['title'], 'name': 'dromouse'}，将p标签的属性打印出来，典型的字典类型，可以获取键值。
+    print(soup.p["class"])  # title 获取字典的某个属性
+    # 2、NavigableString 已经得到标签的内容，获取标签中的文字用.string解决
+    print(soup.p.string)  # The Dormouse's story
+    # 3、Beautiful Soup 文档的全部内容
+    # 4、Comment 是一个特殊类型的NavigableString类型，其输出的内容仍然不包括注释符号，如果不好好处理，对文本会造成麻烦。
+    print(soup.a)  # <a class="sister" href="http://example.com/elsie" id="link1"><!-- Elsie --></a>
+    print(soup.a.string)  # Elsie
+    print(type(soup.a.string))  # <class 'bs4.element.Comment'>
+    # a标签中的内容其实是注释，但我们用.string输出时，已经把注释符号去掉了，因此在使用前最好判断是不是Comment类型
+    if type(soup.a.string) == bs4.element.Comment:
+        print(soup.a.string)  # 判断之后进行相应的操作
+
+
+def trave_test():
+    """
+    遍历文档树
+    """
+    html = '''
+        <html><head><title>The Dormouse's story</title></head>
+        <body>
+        <p class="title"><b>The Dormouse's story</b></p>
+
+        <p class="story">Once upon a time there were three little sisters; and their names were
+        <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+        <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+        <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+        and they lived at the bottom of a well.</p>
+        <p class="story">...</p>
+        '''
+    soup = BeautifulSoup(html, 'lxml')
+    # 1、直接子节点：.contents 和 .children属性
+    print(soup.head.contents)  # tag的.content属性可以将tag的子节点以列表的形式输出，[<title>The Dormouse's story</title>]
+    print(soup.head.contents[0])  # 可以用列表索引来获取它的某一个元素
+    print(soup.head.children)  # <list_iterator object at 0x000002C89BCA1CF8>,我们可以发现他是一个list生成器对象
+    # 可以通过遍历获得里面的内容
+    for child in soup.body.children:
+        print(child)
+    # 2、所有子孙节点：.descendants属性
+    for child in soup.descendants:
+        print(child)  # 打印出了所有的节点
+    # 3、多个内容: .string和.stripped_strings属性
+    for string in soup.strings:
+        print(repr(string))  # 获取里面的内容
+    for string in soup.stripped_strings:
+        print(repr(string))  # 输出的字符串中可能包含很多空格或空行，使用stripped_strings可以除多余空白内容
+    # 4、父节点和全部父节点
+    p = soup.p
+    print(p.parent.name)  # body
+    # 全部父节点
+    content = soup.head.title.string
+    for parent in content.parents:
+        print(parent.name)
+    # 5、兄弟节点
+    print(soup.a.next_siblings)  # 获取后面的兄弟节点
+    print(soup.a.previous_siblings)  #获取前面的兄弟节点
+    print(soup.a.next_sibling)  # 获取下一个兄弟标签
+    print(soup.a.previous_sinbling)  # 获取上一个兄弟标签
+    # 6、前后节点
+    print(soup.head.next_element)  # <title>The Dormouse's story</title>
+    # 通过.next_elements和.previous_elements的迭代器就可以向前或向后访问文档的解析内容
+    for element in soup.head.next_elements:
+        print(repr(element))
+
+
+def serach_test():
+    """
+    搜索文档树
+    以find_all( name , attrs , recursive , text , **kwargs )为例
+    """
+    
+
+
+if __name__ == '__main__':
+    trave_test()
